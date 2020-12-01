f5c29e72f00169a5275ca99acc44115e85bb8f50
Nightmare-MY <906262255@qq.com>
2020-12-01 20:44:41 +0800

    Update battery_pretend.dart

--- lib/src/modify_system/battery_pretend.dart
+++ lib/src/modify_system/battery_pretend.dart
@@ -10,139 +10,180 @@ class BatteryPretend extends StatefulWidget {
 }
 
 class _BatteryPretendState extends State<BatteryPretend> {
-  final TextEditingController _textcontroller0 = TextEditingController();
+  final TextEditingController textEditingController = TextEditingController();
   @override
   void initState() {
-    _textcontroller0.text = '';
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 240,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(bottom: 20),
-                child: Text(
-                  '电池伪装',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
+    return Material(
+      color: Colors.white,
+      child: FullHeightListView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    '电池伪装',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              TextField(
-                controller: _textcontroller0,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: '输入要伪装的电量',
-                  contentPadding: EdgeInsets.only(top: 10.0),
-                ),
-                autofocus: false,
-                onSubmitted: (_) {
-                  NiToast.showToast('按确认修改生效');
-                },
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              FlatButton(
-                child: const Text(
-                  '无线充电',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.bold,
+                TextField(
+                  controller: textEditingController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: '输入要伪装的电量',
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    isDense: true,
+                    // enabledBorder: OutlineInputBorder(
+                    //   borderSide: const BorderSide(
+                    //     width: 1,
+                    //     color: Colors.black,
+                    //   ),
+                    //   borderRadius: BorderRadius.circular(8),
+                    // ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Colors.transparent,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Colors.black.withOpacity(0.2),
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Colors.black.withOpacity(0.2),
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    fillColor: const Color(0xfff7f7f7),
+                    filled: true,
                   ),
+                  autofocus: false,
+                  onSubmitted: (_) {
+                    NiToast.showToast('按确认修改生效');
+                  },
                 ),
-                onPressed: () async {
-                  await NiProcess.exec('dumpsys battery set status 2\n'
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                FlatButton(
+                  child: const Text(
+                    '无线充电',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () async {
+                    await NiProcess.exec(
+                      'dumpsys battery set status 2\n'
                       'dumpsys battery set ac 0\n'
                       'dumpsys battery set usb 0\n'
-                      'dumpsys battery set wireless 1\n');
-                  if (widget.callback != null) {
-                    widget.callback();
-                  }
-                },
-              ),
-              FlatButton(
-                child: const Text(
-                  '交流电充电',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+                      'dumpsys battery set wireless 1\n',
+                    );
+                    if (widget.callback != null) {
+                      widget.callback();
+                    }
+                  },
                 ),
-                onPressed: () async {
-                  await NiProcess.exec('dumpsys battery set status 2\n'
+                FlatButton(
+                  child: const Text(
+                    '交流电充电',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () async {
+                    await NiProcess.exec(
+                      'dumpsys battery set status 2\n'
                       'dumpsys battery set ac 1\n'
                       'dumpsys battery set usb 0\n'
-                      'dumpsys battery set wireless 0\n');
-                  if (widget.callback != null) {
-                    widget.callback();
-                  }
-                },
-              ),
-              FlatButton(
-                child: const Text(
-                  'USB充电',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+                      'dumpsys battery set wireless 0\n',
+                    );
+                    if (widget.callback != null) {
+                      widget.callback();
+                    }
+                  },
                 ),
-                onPressed: () async {
-                  await NiProcess.exec('dumpsys battery set status 2\n'
+                FlatButton(
+                  child: const Text(
+                    'USB充电',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () async {
+                    await NiProcess.exec(
+                      'dumpsys battery set status 2\n'
                       'dumpsys battery set ac 0\n'
                       'dumpsys battery set usb 1\n'
-                      'dumpsys battery set wireless 0\n');
-                  if (widget.callback != null) {
-                    widget.callback();
-                  }
-                },
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              FlatButton(
-                child: const Text(
-                  '未充电',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+                      'dumpsys battery set wireless 0\n',
+                    );
+                    if (widget.callback != null) {
+                      widget.callback();
+                    }
+                  },
                 ),
-                onPressed: () async {
-                  await NiProcess.exec('dumpsys battery set status 1');
-                  if (widget.callback != null) {
-                    widget.callback();
-                  }
-                },
-              ),
-              FlatButton(
-                child: const Text(
-                  '恢复默认',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.bold,
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FlatButton(
+                  child: const Text(
+                    '未充电',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                  onPressed: () async {
+                    await NiProcess.exec('dumpsys battery set status 1');
+                    if (widget.callback != null) {
+                      widget.callback();
+                    }
+                  },
                 ),
-                onPressed: () async {
-                  await NiProcess.exec('dumpsys battery reset');
-                  if (widget.callback != null) {
-                    widget.callback();
-                  }
-                },
-              ),
-              FlatButton(
+                FlatButton(
+                  child: const Text(
+                    '恢复默认',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () async {
+                    await NiProcess.exec('dumpsys battery reset');
+                    if (widget.callback != null) {
+                      widget.callback();
+                    }
+                  },
+                ),
+                FlatButton(
                   child: const Text(
                     '确认修改',
                     style: TextStyle(
@@ -152,14 +193,16 @@ class _BatteryPretendState extends State<BatteryPretend> {
                   ),
                   onPressed: () async {
                     await NiProcess.exec(
-                        'dumpsys battery set level ${int.parse(_textcontroller0.text)}\n');
+                        'dumpsys battery set level ${int.parse(textEditingController.text)}\n');
                     if (widget.callback != null) {
                       widget.callback();
                     }
-                  }),
-            ],
-          )
-        ],
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
